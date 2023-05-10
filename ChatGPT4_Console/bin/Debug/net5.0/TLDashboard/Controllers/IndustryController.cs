ed34e07a484ab23c528d66dd653e6b3d2c4d7e78
rachit-talwar-incedo <130658827+rachit-talwar-incedo@users.noreply.github.com>
2023-05-10 23:23:28 +0530

    Add files via upload
    Initial Commit

---
+++ ChatGPT4_Console/bin/Debug/net5.0/TLDashboard/Controllers/IndustryController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using Areas.TLDashboard.Models;
+using TLMDomain;
+using TLMBLL;
+
+namespace TLM.Areas.TLDashboard.Controllers
+{
+    public class IndustryController : BaseController
+    {
+        // GET: TLDashboard/Industry
+        public ActionResult Index(string tlId)
+        {
+            var id = Convert.ToInt32(Common.DecryptData(tlId != null && tlId != "" ? tlId : "0"));
+            ViewBag.TlId = id;
+            DataSet dsTL = new IndustryInteractionBLL().GetIndustryInteractionInfo(id);
+            List<IndustryInteractionsViewModel> IndustryInteractions = new List<IndustryInteractionsViewModel>();
+
+            if (dsTL != null && dsTL.Tables.Count > 0 && dsTL.Tables[0].Rows.Count > 0)
+            {
+                foreach (DataRow dr in dsTL.Tables[0].Rows)
+                {
+                    IndustryInteractions.Add(new IndustryInteractionsViewModel { IndustryName = dr["CompanyName"].ToString(), ProgramYear = dr["ProgramYear"].ToString() });
+                }
+            }
+            var indStatus = dsTL.Tables[1].Rows[0];
+            var hideStatus = dsTL.Tables[2].Rows[0];
+            if (Convert.ToInt32(hideStatus["II"].ToString()) == 0 && indStatus["HideConf_Presetn"].ToString() == "Yes" && indStatus["HideExternal_Advisory"].ToString() == "Yes" && indStatus["HideEngagement_Activities"].ToString() == "Yes")
+                return RedirectToAction("Index", "Dashboard", new { tlId = tlId });
+            if (Convert.ToInt32(hideStatus["II"].ToString()) == 0)
+            {
+                if (indStatus["HideConf_Presetn"].ToString() == "No")
+                    return RedirectToAction("IndustryActivities", new { tlId = tlId });
+                else if (indStatus["HideExternal_Advisory"].ToString() == "No")
+                    return RedirectToAction("Advisory", new { tlId = tlId });
+                else return RedirectToAction("Speaking", new { tlId = tlId });
+            }
+            return View(IndustryInteractions);
+        }
+
+        #region MyRegion
+        public ActionResult IndustryActivities(string tlId)
+        {
+            var id = Convert.ToInt32(Common.DecryptData(tlId != null && tlId != "" ? tlId : "0"));
+            ViewBag.TlId = id;
+            IndustryActivitiesViewModel IndustryActivities = new IndustryActivitiesViewModel();
+            DataSet dsTL = new ThoughtLeaderBLL().GetGetTLTotalInteractionCount(Convert.ToInt32(ViewBag.TlId));
+
+            if (dsTL != null && dsTL.Tables.Count > 0 && dsTL.Tables[0].Rows.Count > 0)
+            {
+                IndustryActivities.ConferencesCount = dsTL.Tables[0].Rows[0]["ConferencesCount"].ToString();
+                IndustryActivities.InteractionsCount = dsTL.Tables[0].Rows[0]["InteractionsCount"].ToString();
+                IndustryActivities.PresentationsCount = dsTL.Tables[0].Rows[0]["PresentationsCount"].ToString();
+            }
+
+            return View(IndustryActivities);
+        }
+        public ActionResult IndustryCongress(int tlId)
+        {
+            List<IndustryCongressViewModel> IndustryCongress = new List<IndustryCongressViewModel>();
+            DataSet dsTL = new CongressActivityBAL().GetTLCongressActivity(tlId, 1);
+            if (dsTL != null && dsTL.Tables.Count > 0 && dsTL.Tables[0].Rows.Count > 0)
+            {
+                foreach (DataRow dr in dsTL.Tables[0].Rows)
+                {
+                    IndustryCongress.Add(new IndustryCongressViewModel { Count = dr["Count"].ToString(), ThoughtLeaderID = dr["ThoughtLeaderID"].ToString(), Topic = dr["Topic"].ToString() });
+                }
+            }
+            return PartialView(IndustryCongress);
+        }
+       
+        #endregion
+    }
+}
---
+++ ChatGPT4_Console/bin/Debug/net5.0/TLDashboard/Controllers/Program.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Linq;
+
+namespace ChatGPT
+{
+    class Program
+    {
+
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            //Define String builder
+            StringBuilder Result = new StringBuilder();
+
+            string inputfolderPath = Path.Combine(Environment.CurrentDirectory, "TLDashboard");
+
+            string[] allDirectories = Directory.GetDirectories(inputfolderPath, "*", SearchOption.AllDirectories);
+
+            // Create a list to store all file paths
+            List<string> filepaths = new List<string>();
+
+            // Get all files' paths from the specified folder
+            foreach (string file in Directory.GetFiles(inputfolderPath))
+            {
+                filepaths.Add(file);
+            }
+
+            // Loop through all directories and add all file paths to the list
+            foreach (string directory in allDirectories)
+            {
+                string[] files = Directory.GetFiles(directory);
+
+                foreach (string file in files.OrderBy(x => x))
+                {
+                    filepaths.Add(file);
+                }
+            }
+            // Specify the file path where you want to write output text
+            string outputfilePath = Path.Combine(Environment.CurrentDirectory, @"ReportChatGPT\Report-" + DateTime.Now.Millisecond);
+
+            // Loop through all the filepaths and read the content of each file
+            foreach (string filepath in filepaths)
+            {
+                // Read the content of the file
+                string fileContent = string.Concat("Write a html functional requirement output for the below c# code: ", Environment.NewLine, System.IO.File.ReadAllText(filepath));
+                try
+                {
+                    Console.WriteLine(string.Concat("Below is the explaination of the file path code : ", filepath, Environment.NewLine));
+                    Result.Append(string.Concat("<h3>Below is the explaination of the file path code : ", filepath, "</h3>"));
+                    Result.Append(string.Concat("<p>", await ChatGPTCall(fileContent), "</p>"));                   
+                   
+                }
+                catch (System.Exception ex)
+                {                    
+                    Console.WriteLine(ex.Message);
+                    Result.Append(string.Concat("<p>An error occurred for the above code explaination: ", ex.Message, "</p>"));
+                  
+                }
+            }
+
+            string inputhtml = System.IO.File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "template.html"));
+            inputhtml = inputhtml.Replace("##HtmlBody", Result.ToString());
+            //Save html file
+            System.IO.File.WriteAllText(outputfilePath + ".html", inputhtml);
+            //Save html into word doc
+            SaveAsPDF(outputfilePath, inputhtml);
+        }
+
+        public async static Task<string> ChatGPTCall(string input)
+        {
+            //Console.WriteLine("API Request...");
+            //Console.WriteLine(Environment.NewLine);
+            //Console.WriteLine(input);
+            //Console.WriteLine(Environment.NewLine);
+            Console.WriteLine("Fetching API Response...");
+            Console.WriteLine(Environment.NewLine);
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "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");
+
+            var msg = new message() { content = input, role = "user" };
+
+            var request = new OpenAIRequest
+            {
+                Model = "gpt-3.5-turbo",
+                Temperature = 1,
+                messages = new message[1],
+
+            };
+            request.messages[0] = msg;
+
+            var json = JsonSerializer.Serialize(request);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://api.openai.com/v1/chat/completions", content);
+            var resjson = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Received API Response...");
+            if (!response.IsSuccessStatusCode)
+            {
+                //var errorResponse = JsonSerializer.Deserialize<OpenAIErrorResponse>(resjson);                
+                Console.WriteLine("Error: " + resjson);
+                return string.Empty;
+            }
+            else
+            {
+                var data = JsonSerializer.Deserialize<Root>(resjson);
+                var output = string.Join(Environment.NewLine, data.choices.Select(x => x.message).Select(x => x.content).ToList());
+                Console.WriteLine(output);
+                return output;
+            }
+        }
+
+        public static bool SaveAsPDF(string path, string html)
+        {
+            //IGeneratePdf _generatePdf = new GeneratePdf();
+
+            //byte[] fileBytes = _generatePdf.GetPDF(html);
+            //if (fileBytes != null)
+            //{
+            //    System.IO.File.WriteAllBytes(path, fileBytes);
+            //}
+            return true;
+        }
+    }
+
+    public class Choice
+    {       
+        public int index { get; set; }        
+        public string finish_reason { get; set; }
+        public message message { get; set; }
+    }
+    public class Root
+    {
+        public string id { get; set; }
+        public string @object { get; set; }
+        public int created { get; set; }       
+        public List<Choice> choices { get; set; }
+        public Usage usage { get; set; }
+    }
+    public class Usage
+    {
+        public int prompt_tokens { get; set; }
+        public int completion_tokens { get; set; }
+        public int total_tokens { get; set; }
+    }
+
+    public class OpenAIRequest
+    {
+        [JsonPropertyName("model")]
+        public string Model { get; set; }
+
+        public message[] messages { get; set; }
+
+
+        [JsonPropertyName("temperature")]
+        public float Temperature { get; set; }
+    }
+    public class message
+    {
+        [JsonPropertyName("role")]
+        public string role { get; set; }
+        public string content { get; set; }
+    }
+
+}
